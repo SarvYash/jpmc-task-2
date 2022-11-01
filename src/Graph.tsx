fc923956afa446c40bff22b665cc80b2cdc3d19c
SarvYash <116999875+SarvYash@users.noreply.github.com>
2022-11-01 19:49:25 +0530

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -15,9 +15,10 @@ interface IProps {
  * This interface acts as a wrapper for Typescript compiler.
  */
 interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extebds HTMLElement{
   load: (table: Table) => void,
 }
-
+}
 /**
  * React component that renders Perspective based on data
  * parsed from its parent through data property.
@@ -33,14 +34,23 @@ class Graph extends Component<IProps, {}> {
   componentDidMount() {
     // Get element to attach the table from the DOM.
     const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
     const schema = {
       stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
-    };
-
+      
+      
+      elem.load(this.table);
+      ElementInternals.setAttribut('view','y_line');
+      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('roe-pivots', '["timestamp]');
+      elem.setAttribute('colums','["top_ask_price"]');
+      elem.setAttribute('aggregentes',
+      {"stock":"distinct count",
+      top_ask_price: 'avg',
+      top_bid_price: 'avg',
+      "trimStart":"distinct count"});
+    }
+   }
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
